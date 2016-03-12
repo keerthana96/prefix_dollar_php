64ba8533ea652464a76983e042bf90143056b934
Apuurva02 <apuurva97@gmail.com>
2016-03-12 18:01:11 +0000

    modified function

--- Dollar.py
+++ Dollar.py
@@ -1,5 +1,12 @@
 import sublime, sublime_plugin
 
+# class ExecuteCommand(sublime_plugin.EventListener):
+# 	def onModified(self, view):
+# 		getattr(DollarCommand, 'run')
+
+
+
+
 class DollarCommand(sublime_plugin.TextCommand):
 	# read every word
     # if word is a variable and if it does not already exist in the list, add to list
@@ -12,12 +19,21 @@ class DollarCommand(sublime_plugin.TextCommand):
     count=0
     
 	def run(self, edit):
-		wordpos = self.view.word(self.view.sel()[0].end()-1)
-		word = self.view.substr(wordpos)
-		if self.view.substr(wordpos.begin()-1) is "$":
-			if word not in DollarCommand.varlist:
-				DollarCommand.varlist.append(word)
-				
-		elif word in DollarCommand.varlist:
-			for pos in self.view.sel():
-				self.view.insert(edit, wordpos.begin(), '$')
+		# print(self.view.substr(self.view.sel()[0].end()-1))
+		# print(DollarCommand.varlist)
+
+		if self.view.substr(self.view.sel()[0].end()-1) in DollarCommand.stringlist:
+			DollarCommand.count+=1
+			if DollarCommand.count%2==0:
+				DollarCommand.count=0
+
+		if self.view.substr(self.view.sel()[0].end()-1) in DollarCommand.traillist and DollarCommand.count is 0:
+			wordpos = self.view.word(self.view.sel()[0].end()-1)
+			word = self.view.substr(wordpos)
+			if self.view.substr(wordpos.begin()-1) is "$":
+				if word not in DollarCommand.varlist:
+					DollarCommand.varlist.append(word)
+					
+			elif word in DollarCommand.varlist:
+				for pos in self.view.sel():
+					self.view.insert(edit, wordpos.begin(), '$')
