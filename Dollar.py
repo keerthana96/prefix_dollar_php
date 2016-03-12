2d3baf18e090af21389d69c3224ce0c8c853c362
Chandana02 <sshchandana.bitra@gmail.com>
2016-03-12 17:53:58 +0530

    Variables added

--- Dollar.py
+++ Dollar.py
@@ -1,6 +1,16 @@
 import sublime, sublime_plugin
 
 class DollarCommand(sublime_plugin.TextCommand):
+	# read every word
+    # if word is a variable and if it does not already exist in the list, add to list
+    # else check if word exists in list
+        # if yes, prefix $ in front of it 
+        
+    varlist=[]
+    traillist=[" ","="]
+    stringlist=["'",'"']
+    count=0
+    
 	def run(self, edit):
 		wordpos = self.view.word(self.view.sel()[0].end()-1)
 		word = self.view.substr(wordpos)
